a08346833ae442aadbbb17775b6fc50ec586d4c3
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2026-02-25 10:53:26 +0500

    featured queries section added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,11 +2,13 @@
 
 import SectionHeader from "@/components/ui/SectionHeader";
 import ClassesSection from "@/components/teacher/ClassesSection";
+import { FeaturedQueriesSection } from "@/components/student/FeaturedQueriesSection";
 
 export default function StudentPage() {
   return (
     <div className="flex flex-col">
       <SectionHeader title="Overview" />
+      <FeaturedQueriesSection />
       <ClassesSection role={"student"} />
     </div>
   );
