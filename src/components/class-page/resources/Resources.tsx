7755b16e55e280899f395ff50fd163c41324dcec
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2026-02-26 08:07:19 +0500

    resources

---
+++ src/components/class-page/resources/Resources.tsx
@@ -0,0 +1,6 @@
+import { ComingSoon } from "@/components/ui/ComingSoon";
+import { Library } from "lucide-react";
+
+export function Resources() {
+  return <ComingSoon text="Resources Coming Soon" icon={Library} />;
+}
