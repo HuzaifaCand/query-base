3e77060139b8b02a290bb22f8f47ab90df8948d9
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2026-02-26 08:07:30 +0500

    huge coming soon comp

---
+++ src/components/ui/ComingSoon.tsx
@@ -0,0 +1,17 @@
+export function ComingSoon({
+  text,
+  icon,
+}: {
+  text: string;
+  icon: React.ElementType;
+}) {
+  const Icon = icon;
+  return (
+    <div className="border-1 border-primary/10 text-muted-foreground flex items-center justify-center ">
+      <div className="py-12 px-4 flex flex-col gap-4 items-center">
+        <Icon className="w-8 h-8 text-textMuted" />
+        <p className="text-xs sm:text-sm">{text}</p>
+      </div>
+    </div>
+  );
+}
